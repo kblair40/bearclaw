3facaa6eb1413c6e902dd556ce1dfbf2ab39ba17
Kevin Blair <kblair40@gmail.com>
2022-11-28 20:14:48 -0600

    Add/style InputColumn helper component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,6 +5,9 @@ import { FormContext } from "../../store/FormContext";
 import LinkedinConnection from "../LinkedInConnection";
 import Avatar from "../Avatar";
 import Switch from "../Switch";
+import Stack from "../Stack";
+import Select from "../Select";
+import Input from "../Input";
 
 const Form = () => {
   const { saving, theme, version, onToggleTheme, onToggleVersion, onSubmit } =
@@ -43,8 +46,67 @@ const Form = () => {
           </div>
         </div>
       </div>
+
+      <div className="form__row form__row--bottom">
+        <InputColumn colNumber={1} />
+        <InputColumn colNumber={2} />
+
+        <div className="form__input-column"></div>
+      </div>
     </form>
   );
 };
 
 export default Form;
+
+const COLUMN_INPUTS = {
+  1: FORM_INPUTS.slice(0, 4),
+  2: FORM_INPUTS.slice(4),
+};
+
+type InputColumnProps = {
+  colNumber: 1 | 2 | "1" | "2";
+};
+
+const InputColumn = ({ colNumber }: InputColumnProps) => {
+  const formCtx = useContext(FormContext);
+  const { onChangeInputValue, onChangeSelectValue, inputValues } = formCtx;
+
+  if (typeof colNumber === "number") {
+    colNumber = `${colNumber}`;
+  }
+
+  const inputs = COLUMN_INPUTS[colNumber];
+
+  let className = "form__input-column";
+  className += ` form__input-column--${colNumber}`;
+
+  return (
+    <div className={className}>
+      <Stack>
+        {inputs.map((input, i) => {
+          const { nameAndId, label, options, type } = input;
+          const commonProps = {
+            key: i,
+            value: inputValues.nameAndId,
+            nameAndId,
+            label,
+          };
+          return options ? (
+            <Select
+              {...commonProps}
+              onChange={onChangeSelectValue}
+              options={options}
+            />
+          ) : (
+            <Input
+              {...commonProps}
+              onChange={onChangeInputValue}
+              type={type!}
+            />
+          );
+        })}
+      </Stack>
+    </div>
+  );
+};
